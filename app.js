5a4ed511766f39b838c11d529734042fe95a0ab7
Mario <malux97@hotmail.com>
2023-11-07 09:59:42 -0600

    Despedida personalizada

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ app.get('/despedida', function(req, res) {
     res.send('Adiós mundo cruel!');
 });
 
+app.get('/despedida/familia', function(req, res) {
+    res.send('Los quiero mucho a todos!');
+});
+
+app.get('/despedida/empleados', function(req, res) {
+    res.send('Ojalá se rompan una pierna!');
+});
+
 app.listen(3000, function(){
     console.log('Aplicación ejemplo, escuchando el puerto 3000!');
 });
